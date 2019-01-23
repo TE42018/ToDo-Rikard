93835474e9a39b7561f987b04a879ee92215ff93
Rikard Larsson <rikard.larsson@elev.ga.ntig.se>
2019-01-23 13:37:55 +0100

    commit

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -12,13 +12,13 @@ export class ListComponent implements OnInit {
   list: Task[];
 
   constructor() {
-    // this.list = [
-    //   {title: 'Title 1', state: 'To Do', description: 'description 1', urgent: true },
-    //   {title: 'Title 2', state: 'Done', description: 'description 2', urgent: true },
-    //   {title: 'Title 3', state: 'In Progress', description: 'description 3', urgent: false },
-    //   {title: 'Title 4', state: 'To Do', description: 'description 4', urgent: true },
-    // ];
-    
+    this.list = [
+      {title: 'Title 1', state: 'To Do', description: 'description 1', urgent: true },
+      {title: 'Title 2', state: 'Done', description: 'description 2', urgent: true },
+      {title: 'Title 3', state: 'In Progress', description: 'description 3', urgent: false },
+      {title: 'Title 4', state: 'To Do', description: 'description 4', urgent: true },
+    ];
+
   }
 
 
